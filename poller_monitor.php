c5e871383461aae0a7704a8c993ec638d21e64b1
Jimmy Conner <jimmy@sqmail.org>
2018-08-30 06:58:38 -0500

    Removing reference to reports API

--- poller_monitor.php
+++ poller_monitor.php
@@ -38,7 +38,6 @@ $poller_start = microtime(true);
 $start_date   = date('Y-m-d H:i:s');
 
 include('../../include/global.php');
-include_once($config['base_path'] . '/lib/reports.php');
 
 define('BR', "\n");
 
