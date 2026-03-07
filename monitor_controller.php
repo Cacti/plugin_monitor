792904e23142de3a72bf6bcf4fc624655fad5d41
TheWitness <thewitness@cacti.net>
2026-03-07 12:55:02 -0500

    fix: Ensure that check_all_pages.sh succeeds in Cacti (#203)

--- monitor_controller.php
+++ monitor_controller.php
@@ -110,9 +110,9 @@ function drawPage(): void {
 
 	if (function_exists($function) && get_request_var('view') != 'list') {
 		if (get_request_var('grouping') == 'default' || get_request_var('grouping') == 'site') {
-			html_start_box(__('Monitored Devices', 'monitor'), '100%', true, '3', 'center', '');
+			html_start_box(__('Monitored Devices', 'monitor'), '100%', true, 3, 'center', '');
 		} else {
-			html_start_box('', '100%', true, '3', 'center', '');
+			html_start_box('', '100%', true, 3, 'center', '');
 		}
 		print $function();
 	} else {
@@ -639,7 +639,7 @@ function drawFilterAndStatus(): void {
 
 	$header = __('Monitor Filter [ Last Refresh: %s ]', date('g:i:s a', time()), 'monitor') . (get_request_var('refresh') < 99999 ? __(' [ Refresh Again in <i style="padding:0px !important;margin:0px;" id="timer">%d</i> Seconds ]', get_request_var('refresh'), 'monitor') : '') . (get_request_var('view') == 'list' ? __('[ Showing only first 30 Devices ]', 'monitor') : '') . '<span id="text" style="vertical-align:baseline;padding:0px !important;display:none"></span>';
 
-	html_start_box($header, '100%', false, '3', 'center', '');
+	html_start_box($header, '100%', false, 3, 'center', '');
 
 	print '<tr class="even"><td>' . PHP_EOL;
 	print '<form class="monitorFilterForm">' . PHP_EOL;
--- monitor_render.php
+++ monitor_render.php
@@ -964,7 +964,7 @@ function renderHeaderList(int $total_rows = 0, int $rows = 0): string {
 
 	$nav = html_nav_bar('monitor.php?rfilter=' . get_request_var('rfilter'), MAX_DISPLAY_PAGES, get_request_var('page'), $rows, $total_rows, 12, __('Devices'), 'page', 'main');
 
-	html_start_box(__('Monitored Devices', 'monitor'), '100%', false, '3', 'center', '');
+	html_start_box(__('Monitored Devices', 'monitor'), '100%', false, 3, 'center', '');
 
 	print $nav;
 
